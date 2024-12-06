c54601722d987327f059caf83b1b82b71471a70d
Ngô Huy Hoàn <113993178+nhuyhoan004@users.noreply.github.com>
2024-12-06 16:35:20 +0700

    solution 1
    solution 1

---
+++ 2D_Convolution/vivado_hls.app
@@ -0,0 +1,20 @@
+<project xmlns="com.autoesl.autopilot.project" name="2D_Convolution" top="doImgproc">
+    <files>
+        <file name="core.cpp" sc="0" tb="false" cflags=""/>
+        <file name="../../a.bmp" sc="0" tb="1" cflags=""/>
+        <file name="../../ab.bmp" sc="0" tb="1" cflags=""/>
+        <file name="../../c.bmp" sc="0" tb="1" cflags=""/>
+        <file name="../../filter_result.bmp" sc="0" tb="1" cflags=""/>
+        <file name="../../filter_result.jpg" sc="0" tb="1" cflags=""/>
+        <file name="../../test_bench.cpp" sc="0" tb="1" cflags=""/>
+    </files>
+    <includePaths/>
+    <libraryPaths/>
+    <Simulation>
+        <SimFlow name="csim" csimMode="0" lastCsimMode="2"/>
+    </Simulation>
+    <solutions xmlns="">
+        <solution name="solution1" status="active"/>
+    </solutions>
+</project>
+
---
+++ core.h
@@ -0,0 +1,31 @@
+#include "hls_video.h"
+
+//Defination of size of image
+#define IMG_WIDTH_OR_COLS 240
+#define IMG_HEIGHT_OR_ROWS 320
+
+// 3x3 kernel
+#define KERNEL_DIM 3
+
+//Image file path
+#define INPUT_IMAGE_CORE   "./ab.bmp"
+#define INPUT_IMAGE_2      "./a.bmp"
+#define INPUT_IMAGE_3      "./c.bmp"
+#define OUTPUT_IMAGE_CORE  "./filter_result.bmp"
+#define OUTPUT_IMAGE_REF   "./filter_resultRef.bmp"
+
+
+//Use the axi stream side-channel
+#include <ap_axi_sdata.h>
+typedef ap_axiu<8,2,5,6> uint_8_side_channel;
+typedef ap_axis<8,2,5,6> int_8_side_channel;
+
+//Reference functions
+short convolve2d(hls::Window<KERNEL_DIM,KERNEL_DIM,char>*window, char kernel[KERNEL_DIM*KERNEL_DIM]);
+
+
+//IP
+void doImgproc(hls::stream<uint_8_side_channel> &inStream, hls::stream<int_8_side_channel> &outStream, char kernel[KERNEL_DIM*KERNEL_DIM],int operation);
+//Parameters: 1. Input stream 2. Output stream 3. Kernel 4. Operation
+short sumWindow(hls::Window<KERNEL_DIM,KERNEL_DIM,short>*window);
+ 
